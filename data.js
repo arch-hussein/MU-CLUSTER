f52b312b50b110f5ce7561ff5a2ad84feb0dd7d5
arch-hussein <xamar381@gmail.com>
2025-05-23 21:10:13 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,516 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-ground-floor",
+      "name": "GROUND FLOOR",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.366006323871792,
+          "pitch": -0.014887774692120104,
+          "rotation": 0,
+          "target": "3-pharmacy"
+        },
+        {
+          "yaw": -0.8533479669392072,
+          "pitch": 0.09063691574836241,
+          "rotation": 0,
+          "target": "4-dental-clinic"
+        },
+        {
+          "yaw": -0.38875776685343766,
+          "pitch": 0.019878722501196933,
+          "rotation": 0,
+          "target": "13-obs-and-gyn"
+        },
+        {
+          "yaw": -0.2204795604988945,
+          "pitch": 0.05477403047306595,
+          "rotation": 0,
+          "target": "6-internal-medicine"
+        },
+        {
+          "yaw": 0.43009556192289367,
+          "pitch": 0.021139390261623703,
+          "rotation": 0,
+          "target": "7-lab"
+        },
+        {
+          "yaw": 0.6239873858491727,
+          "pitch": 0.09511791358355381,
+          "rotation": 0,
+          "target": "5-opd-clinic-"
+        },
+        {
+          "yaw": 3.1051062268528637,
+          "pitch": -0.1752043952704856,
+          "rotation": 0,
+          "target": "9-first-floor-reception"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-ground-floor-reception",
+      "name": "GROUND FLOOR RECEPTION",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 877,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-receptionist-view",
+      "name": "RECEPTIONIST VIEW",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 877,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6248534953519975,
+          "pitch": 0.10367629439660853,
+          "rotation": 0,
+          "target": "5-opd-clinic-"
+        },
+        {
+          "yaw": 1.0820827802167763,
+          "pitch": 0.0001071034870907539,
+          "rotation": 0,
+          "target": "4-dental-clinic"
+        },
+        {
+          "yaw": 0.43283847025778854,
+          "pitch": -0.0025265965610881125,
+          "rotation": 0,
+          "target": "3-pharmacy"
+        },
+        {
+          "yaw": -0.01700251742048664,
+          "pitch": -0.054880410285129244,
+          "rotation": 0,
+          "target": "9-first-floor-reception"
+        },
+        {
+          "yaw": -2.132610430113436,
+          "pitch": -0.052501692817113366,
+          "rotation": 4.71238898038469,
+          "target": "7-lab"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-pharmacy",
+      "name": "pharmacy",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-dental-clinic",
+      "name": "DENTAL CLINIC",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-opd-clinic-",
+      "name": "OPD CLINIC ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 877,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0654577796557145,
+          "pitch": 0.12323906343984525,
+          "rotation": 0,
+          "target": "1-ground-floor-reception"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-internal-medicine",
+      "name": "INTERNAL MEDICINE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 877,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-lab",
+      "name": "LAB",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1866929590091626,
+          "pitch": -0.0013856244252679062,
+          "rotation": 0,
+          "target": "2-receptionist-view"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-scene-9",
+      "name": "Scene 9",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-first-floor-reception",
+      "name": "FIRST FLOOR RECEPTION",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 1.2471141406535455,
+        "pitch": -0.10367821868632454,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9815921957731568,
+          "pitch": -0.05418984001907745,
+          "rotation": 4.71238898038469,
+          "target": "11-meeting"
+        },
+        {
+          "yaw": -1.6367396074104512,
+          "pitch": -0.0012502218555994205,
+          "rotation": 5.497787143782138,
+          "target": "10-dean-office-sample"
+        },
+        {
+          "yaw": -2.3930957415498533,
+          "pitch": -0.07845264979697575,
+          "rotation": 3.141592653589793,
+          "target": "1-ground-floor-reception"
+        },
+        {
+          "yaw": 2.7885578486486455,
+          "pitch": -0.13536347672526006,
+          "rotation": 0,
+          "target": "12-vip-lounge"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-dean-office-sample",
+      "name": "DEAN OFFICE SAMPLE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6034873229971387,
+          "pitch": 0.14425447145742787,
+          "rotation": 1.5707963267948966,
+          "target": "9-first-floor-reception"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-meeting",
+      "name": "MEETING",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3984523912881706,
+          "pitch": -0.0097588581588397,
+          "rotation": 0,
+          "target": "9-first-floor-reception"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-vip-lounge",
+      "name": "VIP LOUNGE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-obs-and-gyn",
+      "name": "obs and gyn",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 877,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "MU UPDATE",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
